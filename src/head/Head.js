6d13c2468fd00682a6036f4da43674655f9927d3
Ravi727570 <148647125+Ravi727570@users.noreply.github.com>
2025-06-13 16:43:33 +0530

    Update Head.js

--- src/head/Head.js
+++ src/head/Head.js
@@ -8,7 +8,7 @@ const Head = ({ onAdd, candies, onBuy }) => {
 
   const handleAdd = () => {
     if (!name || !text || !price) return;
-    onAdd({ name, description: text, price: parseFloat(price)});
+    onAdd({ name, description: text, price: parseFloat(price) });
     setName('');
     setText('');
     setPrice('');
@@ -16,30 +16,34 @@ const Head = ({ onAdd, candies, onBuy }) => {
 
   return (
     <div className="head">
+      <h2>Candy Shop</h2>
       <div className="datas">
-        <label>
-          Candy Name
-          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-        </label>
-        <label>
-          Description
-          <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
-        </label>
-        <label>
-          Price
-          <input type="text" value={price} onChange={(e) => setPrice(e.target.value)} />
-        </label>
-        <button className="btn" onClick={handleAdd}>Add</button>
-      </div>
+  <div className="form-group">
+    <label>Candy Name</label>
+    <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+  </div>
+  <div className="form-group">
+    <label>Description</label>
+    <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+  </div>
+  <div className="form-group">
+    <label>Price</label>
+    <input type="text" value={price} onChange={(e) => setPrice(e.target.value)} />
+  </div>
+  <button className="btn" onClick={handleAdd}>Add</button>
+</div>
 
+<br></br>
+<hr></hr>
       <ul className="candy-list">
         {candies.map(candy => (
+          
           <li key={candy.id}>
-            <strong>{candy.name}</strong> - {candy.description} - ₹{candy.price}
             <div className="saveDatas">
-              <button className="btn1" disabled={candy.quantity < 1} onClick={() => onBuy(candy.id, 1)}>Buy One</button>
-              <button className="btn1" disabled={candy.quantity < 2} onClick={() => onBuy(candy.id, 2)}>Buy Two</button>
-              <button className="btn1" disabled={candy.quantity < 3} onClick={() => onBuy(candy.id, 3)}>Buy Three</button>
+            <strong>{candy.name}</strong>   {candy.description}   {candy.price}
+              <button className="btn1" onClick={() => onBuy(candy.id, 1)}>Buy One</button>
+              <button className="btn1" onClick={() => onBuy(candy.id, 2)}>Buy Two</button>
+              <button className="btn1" onClick={() => onBuy(candy.id, 3)}>Buy Three</button>
             </div>
           </li>
         ))}
